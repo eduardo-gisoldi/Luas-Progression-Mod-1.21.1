f09e9fb1a168d0a9117ac3c6c98b5c750d1e0b72
Luis Eduardo L. Gisoldi <eduardogisoldi@gmail.com>
2026-04-18 16:40:59 -0300

    Initial commit!

---
+++ src/main/java/gisoldi/progression/LuasprogressionClient.java
@@ -0,0 +1,10 @@
+package gisoldi.progression;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LuasprogressionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
